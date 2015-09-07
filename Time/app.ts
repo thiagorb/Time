7149d618766fe509c93b27e5e72b809c96c53722
Thiago Barcala <thiago.barcala@gmail.com>
2015-09-07 20:20:35 +0200

    Adding user interface.

--- Time/app.ts
+++ Time/app.ts
@@ -1,4 +1,20 @@
-﻿window.onload = () => {
-    var gameView = new Time.GameView(<HTMLCanvasElement>document.getElementById("canvas"));
-    gameView.start();
+﻿function gotoView(viewId: string) {
+    var activeView = <HTMLDivElement>document.getElementsByClassName("active")[0];
+    var newView = <HTMLDivElement>document.getElementById(viewId);
+    activeView.style.opacity = "0";
+    newView.style.opacity = "0";
+    newView.classList.add("active");
+    setTimeout(function () {
+        activeView.classList.remove("active");
+        newView.style.opacity = "1";
+    }, 250);
+}
+
+window.onload = () => {
+    //var gameView = new Time.GameView(<HTMLCanvasElement>document.getElementById("canvas"));
+    //gameView.start();
+    
+    document.getElementById("btnStart").addEventListener("click", function () {
+        gotoView("level-selection");
+    });
 };
