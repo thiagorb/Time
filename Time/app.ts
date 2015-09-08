a876386e88e4fa12f8abcdc8fcb3b9e6d158a850
Thiago Barcala <thiago.barcala@gmail.com>
2015-09-09 00:35:16 +0200

    Add functions to the window load event scope in order to make their names minifyable.

--- Time/app.ts
+++ Time/app.ts
@@ -1,20 +1,73 @@
-﻿function gotoView(viewId: string) {
-    var activeView = <HTMLDivElement>document.getElementsByClassName("active")[0];
-    var newView = <HTMLDivElement>document.getElementById(viewId);
-    var dialog = <HTMLDivElement>document.getElementById("dialog");
-    dialog.style.opacity = "0";
-    setTimeout(function () {
-        newView.classList.add("active");
-        activeView.classList.remove("active");
-        dialog.style.opacity = "1";
-    }, 250);
+﻿interface LevelStorageData {
+    time: number;
+    stars: number;
 }
 
-var starsHTML = [0, 0, 0, 0, 0].map(function () {
-    return"<svg viewBox='0 0 94 90'><polygon points='47,0 76,90 0,34 94,34 17,90'></polygon></svg>";
-}).join("");
-
 window.onload = () => {
+    function gotoView(viewId: string) {
+        var activeView = <HTMLDivElement>document.getElementsByClassName("active")[0];
+        var newView = <HTMLDivElement>document.getElementById(viewId);
+        var dialog = <HTMLDivElement>document.getElementById("dialog");
+        dialog.style.opacity = "0";
+        setTimeout(function () {
+            newView.classList.add("active");
+            activeView.classList.remove("active");
+            dialog.style.opacity = "1";
+        }, 250);
+    }
+    
+    var starsHTML = [0, 0, 0, 0, 0].map(function () {
+        return"<svg viewBox='0 0 94 90'><polygon points='47,0 76,90 0,34 94,34 17,90'></polygon></svg>";
+    }).join("");
+        
+    
+    function createTree(data: Array<any>) : Node {
+        var element = document.createElement(data[0]);
+        if (data[1]) {
+            for (var key in data[1]) {
+                element.setAttribute(key, data[1][key]);
+            }
+        }
+        if (typeof(data[2]) == "string") {
+            element.innerHTML = data[2];
+        } else if (data[2]) {
+            data[2].forEach(function (child) { element.appendChild(createTree(child)) });
+        }
+        return element;
+    }
+    
+    function ancestor(element: HTMLElement, tagName: string) {
+        while (element && element.tagName != tagName) {
+            element = element.parentElement;
+        }
+        return element;
+    }
+
+    function registerScore(levelId: number, time: number, stars: number) {
+        var currentData = getLevelStorageData(levelId);
+        if (currentData.time < time) return;
+        
+        localStorage.setItem("score-level-" + levelId, JSON.stringify({
+            time: time,
+            stars: stars
+        }));
+        var levelTr = <HTMLTableRowElement>document.querySelector("tr[data-id=" + levelId + "]");
+        var starsTd = <HTMLTableDataCellElement>levelTr.children[1];
+        starsTd.className = "stars star-" + stars;
+        
+        var timeTd = <HTMLTableDataCellElement>levelTr.children[2];
+        timeTd.innerHTML = formatTime(time);
+    }
+    
+    function formatTime(time: number) {
+        if (time == Infinity) return "---";
+        return time.toFixed(2) + "s";
+    }
+    
+    function getLevelStorageData(levelId: number) : LevelStorageData {
+        return JSON.parse(localStorage.getItem("score-level-" + levelId)) || { stars: null, time: Infinity };
+    }
+    
     var gameView = new Time.GameView(<HTMLCanvasElement>document.getElementById("canvas"));
     gameView.start();
     gameView.gameOverCallback = function () {
@@ -55,55 +108,3 @@ window.onload = () => {
         }, 500);
     })
 };
-
-function createTree(data: Array<any>) : Node {
-    var element = document.createElement(data[0]);
-    if (data[1]) {
-        for (var key in data[1]) {
-            element.setAttribute(key, data[1][key]);
-        }
-    }
-    if (typeof(data[2]) == "string") {
-        element.innerHTML = data[2];
-    } else if (data[2]) {
-        data[2].forEach(function (child) { element.appendChild(createTree(child)) });
-    }
-    return element;
-}
-
-function ancestor(element: HTMLElement, tagName: string) {
-    while (element && element.tagName != tagName) {
-        element = element.parentElement;
-    }
-    return element;
-}
-
-interface LevelStorageData {
-    time: number;
-    stars: number;
-}
-
-function registerScore(levelId: number, time: number, stars: number) {
-    var currentData = getLevelStorageData(levelId);
-    if (currentData.time < time) return;
-    
-    localStorage.setItem("score-level-" + levelId, JSON.stringify({
-        time: time,
-        stars: stars
-    }));
-    var levelTr = <HTMLTableRowElement>document.querySelector("tr[data-id=" + levelId + "]");
-    var starsTd = <HTMLTableDataCellElement>levelTr.children[1];
-    starsTd.className = "stars star-" + stars;
-    
-    var timeTd = <HTMLTableDataCellElement>levelTr.children[2];
-    timeTd.innerHTML = formatTime(time);
-}
-
-function formatTime(time: number) {
-    if (time == Infinity) return "---";
-    return time.toFixed(2) + "s";
-}
-
-function getLevelStorageData(levelId: number) : LevelStorageData {
-    return JSON.parse(localStorage.getItem("score-level-" + levelId)) || { stars: null, time: Infinity };
-}
