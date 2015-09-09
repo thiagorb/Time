27866b7516017b0f232082ca0b3d55dd7a4e3ca7
Thiago Barcala <thiago.barcala@gmail.com>
2015-09-09 19:14:39 +0200

    Simplify deploy.

--- make.sh
+++ make.sh
@@ -30,23 +30,6 @@ echo dist.tar.gz created. Size: `du -b dist.tar.gz | cut -f1` bytes
 
 if [ "$1" == "--deploy" ]
 then
-    if [ "$(git status --porcelain)" ] && [ "$2" != "--force" ]
-    then
-        echo There are uncommited changes, not deploying.
-        exit
-    fi
-    cp dist.tar.gz /var/tmp
     cd "$PROJECTDIR"
-    git branch -d gh-pages
-    git push origin :gh-pages
-    git checkout --orphan gh-pages
-    git reset --hard
-    git clean -df
-    tar -xf /var/tmp/dist.tar.gz --strip-components 1
-    git add -A 
-    git commit -m "Dist"
-    git push -u origin gh-pages
-    git checkout master
-    git reset --hard
-    git clean -df
+    git subtree push --prefix Time/dist origin gh-pages
 fi
